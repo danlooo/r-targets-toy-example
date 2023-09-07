99fa4077e588ffacdd59ce8d0f6cec20ecea4213
Daniel Loos <dloos@bgc-jena.mpg.de>
2023-09-07 09:36:13 +0200

    Fix .Rprofile not loaded properly in targets

--- src/_targets.R
+++ src/_targets.R
@@ -1,5 +1,16 @@
 #!/usr/bin/env R
 
+# ensure tidyverse overwrites base masking
+library(base)
+library(tidyverse)
+library(targets)
+
+# load library scripts
+list.files("src", recursive = TRUE, pattern = ".R$", full.names = TRUE) |>
+  discard(~ .x == "src/_targets.R") |>
+  walk(source)
+
+# List of targets
 list(
   # import external raw data files ----
   tar_target(samples_file, "raw/samples.csv", format = "file"),
